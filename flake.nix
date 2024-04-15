b08a35ce7f3e67b3eb6806556e81d45c72d3edd0
Santiago Cézar <santiagocezar2013@gmail.com>
2024-04-15 13:31:38 -0300

    got the wrong tag

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
         name = "ps2dev";
         src = pkgs.dockerTools.pullImage {
           imageName = "ps2dev/ps2dev";
-          imageDigest = "sha256:5145529beab551bed561442094711211d467299c88054470e3d2d1c05b90a442";
-          sha256 = "12viapmc07a840z1hwsgw9zl06w6z7c6908jdx02h9q0rxin2hdp";
+          imageDigest = "sha256:5b1e631d2fa8be5b00d8283f0c54e4e26e6bf562cfdb95704a8a6bf0b519283e";
+          sha256 = "07av23kmcgv7cib2ki04h5xrhq7rgkjispvgg1pabpf8lx7bb51d";
           finalImageName = "ps2dev/ps2dev";
           finalImageTag = "latest";
         };
